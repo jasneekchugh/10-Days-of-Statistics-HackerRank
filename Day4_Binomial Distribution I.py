40f38a8143f53c0e80281b8e27cf1bb10ff6a335
Jasneek chugh <chughjasneek@gmail.com>
2020-06-23 17:38:15 -0400

    Binomial Distribution I

---
+++ Day4_Binomial Distribution I.py
@@ -0,0 +1,13 @@
+import math
+
+#definied function 'binomial' to calculated using binomial experiment formula
+def binomial(x, n, p):
+    b = (math.factorial(n)/(math.factorial(x)*math.factorial(n-x)))*(p**x)*((1-p)**(n-x))
+    return(b)
+
+b=0
+p=1.09/2.09 #p= prob of success
+n= 6 #n= total no of trials
+for i in range(3,7):
+    b += binomial(i, n, p)   
+print(round(b,3))

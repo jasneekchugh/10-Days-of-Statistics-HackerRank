e3b269a88a68b4bb0a7bff09650486b69ea648e6
Jasneek chugh <chughjasneek@gmail.com>
2020-06-28 18:09:05 -0400

    Update Day4_Binomial Distribution II.py

--- Day4_Binomial Distribution II.py
+++ Day4_Binomial Distribution II.py
@@ -7,5 +7,6 @@ for i in range(10):
         p+=m.factorial(10)/(m.factorial(10-i)*m.factorial(i))*(0.12**i)*(0.88**(10-i))
     if(i>=2):
         p1+=m.factorial(10)/(m.factorial(10-i)*m.factorial(i))*(0.12**i)*(0.88**(10-i))
+        
 print(round(p,3))
 print(round(p1,3))

dbd2ddc77f85da0696a219543e5ce5d2ea6cefe5
Jasneek chugh <chughjasneek@gmail.com>
2020-06-30 22:30:50 -0400

    Poisson Distribution II

---
+++ Day5_Poisson Distribution II.py
@@ -0,0 +1,8 @@
+
+lambdas = list(map(float, input().split()))
+lambda1 = lambdas[0]
+lambda2 = lambdas[1]
+
+# Gets the result and show on the screen
+print (round(160 + 40 * (lambda1 + lambda1 ** 2), 3))
+print (round(128 + 40 * (lambda2 + lambda2 ** 2), 3))
